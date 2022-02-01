febc3a2e2554e01da646d0b759b7a58a25dd87c7
Soumaya <soumaya.ikhrichi5@etu.univ-lorraine.fr>
2022-02-01 11:43:50 +0100

    création du package monde avec la classe Monde

--- src/main/java/twisk/Main.java
+++ src/main/java/twisk/Main.java
@@ -4,15 +4,17 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+//extends Application
+public class Main {
+        /*
+        @Override
+        public void start(Stage stage) throws IOException {
+            stage.setTitle("Hello!");
+            stage.show();
+        }
 
-public class Main extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        stage.setTitle("Hello!");
-        stage.show();
+         */
+        public static void main(String[] args) {
+            //launch();
+        }
     }
-
-    public static void main(String[] args) {
-        launch();
-    }
-}
