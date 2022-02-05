49b9c3eeac51b845db084389664e2da0dbd78f71
Rawan Al-Ameri <rawan.al-ameri9@etu.univ-lorraine.fr>
2022-02-05 11:44:27 +0100

    Classe Test de la classe Etape

---
+++ src/twisk/tests/EtapeTest.java
@@ -0,0 +1,45 @@
+package twisk.tests;
+
+import org.junit.jupiter.api.*;
+import twisk.monde.Activite;
+import twisk.monde.Etape;
+import twisk.monde.Guichet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EtapeTest {
+    Etape FilTicket,Tickets,FileCine,Cine;
+    @BeforeEach
+    void setUp() {
+        FilTicket= new Guichet("File Ticket",4);
+        Tickets =new Activite("Achat Tickets",8,2);
+        FileCine=new Guichet("File Cinema",2);
+        Cine=new Activite("Cinema",7200,60);
+    }
+
+    @Test
+    void ajouterSuccesseur() {
+        FilTicket.ajouterSuccesseur(Tickets,FileCine,Cine);
+        Tickets.ajouterSuccesseur(FileCine,Cine);
+        FileCine.ajouterSuccesseur(Cine);
+        assertEquals(3,FilTicket.nbSuccesseurs());
+        assertEquals(2,Tickets.nbSuccesseurs());
+        assertEquals(1,FileCine.nbSuccesseurs());
+        assertEquals(0,Cine.nbSuccesseurs());
+    }
+
+
+    @Test
+    void estUneActivite() {
+        assertTrue(Cine.estUneActivite());
+        assertTrue(FilTicket.estUnGuichet());
+        assertFalse(FileCine.estUneActivite());
+        assertFalse(Tickets.estUnGuichet());
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("File Ticket: 4 jetons",FilTicket.toString());
+        assertEquals("Cinema: 7200s ± 60s",Cine.toString());
+    }
+}
