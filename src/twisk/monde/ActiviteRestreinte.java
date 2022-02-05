50fe366b06238b9413a6ef9ef4844dc5d409a039
Rawan Al-Ameri <rawan.al-ameri9@etu.univ-lorraine.fr>
2022-02-05 11:51:51 +0100

    Classe Test de la classe Etape

---
+++ src/twisk/monde/ActiviteRestreinte.java
@@ -0,0 +1,17 @@
+package twisk.monde;
+
+public class ActiviteRestreinte extends Activite{
+
+    public ActiviteRestreinte(String nom){
+        super(nom);
+    }
+
+    public ActiviteRestreinte(String nom,int t,int e){
+        super(nom,t,e);
+    }
+
+    @Override
+    public String toString(){
+        return this.nom +": "+this.temps+"s"+ "±"+ ecartTemps;
+    }
+}
