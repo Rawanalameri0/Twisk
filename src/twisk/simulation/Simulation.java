e68af7dc7cf16a9486e9fcdb7276465ea7120763
alameri1u <rawan.al-ameri9@etu.univ-lorraine.fr>
2022-03-08 20:56:07 +0100

    Ajout de la classe kitC avec le repertoire ressources

--- src/twisk/simulation/Simulation.java
+++ src/twisk/simulation/Simulation.java
@@ -2,10 +2,14 @@ package twisk.simulation;
 
 import twisk.monde.Etape;
 import twisk.monde.Monde;
+import twisk.outils.KitC;
 
 public class Simulation {
 
-    public Simulation(){}
+    public Simulation(){
+        KitC kitc = new KitC();
+        kitc.creerEnvironement();
+    }
 
     public void simuler(Monde monde){
         System.out.println(monde.toC());
