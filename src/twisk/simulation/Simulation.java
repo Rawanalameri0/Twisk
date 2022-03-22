218225dc2b39b4d692e49149363810265b724eb8
IKHRICHI Soumaya <soumaya.ikhrichi5@etu.univ-lorraine.fr>
2022-03-22 17:28:17 +0100

    decoupage au niveau de la fonction simuler en ajouter une fonction intermediaire FonctionMain(Monde monde)

--- src/twisk/simulation/Simulation.java
+++ src/twisk/simulation/Simulation.java
@@ -19,39 +19,7 @@ public class Simulation {
         kitc.compiler();
         kitc.construireLibrairie();
         System.load("/tmp/twisk/libTwisk.so");
-        boolean end=false;
-        int nbClientEtape;
-        int nbGuichets=monde.nbGuichets();
-        int nbEtapes=monde.nbEtapes();
-        int[] tabJetons=monde.getTabJetons();
-        int[] tabClients=start_simulation(nbEtapes,nbGuichets,nbClients,tabJetons);
-        System.out.printf("Les clients: ");
-        for (int i=0;i<nbClients;i++){
-            System.out.printf(" "+tabClients[i]+" ");
-        }
-        System.out.println("\n");
-        int[] EmplacementClient;
-        while (!end){
-            EmplacementClient=ou_sont_les_clients(nbEtapes,nbClients);
-            for (int nbClient=0,etape=0;etape<nbEtapes;++nbClient,++etape){
-                nbClientEtape=EmplacementClient[nbClient];
-                System.out.printf("\nétape "+etape+" ("+monde.getNomDeEtape(etape)+") "+EmplacementClient[nbClient]+" clients :");
-                for (int c=1;c<=nbClientEtape;c++){
-                    System.out.printf(" "+EmplacementClient[nbClient+c]+" ");
-                }
-                nbClient+=nbClients;
-                if (etape==1){
-                    end= nbClientEtape==nbClients;
-                }
-            }
-            System.out.println("\n***************************************************************");
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        nettoyage();
+        FonctionMain(monde);
     }
     public native int[] start_simulation(int nbEtapes,int nbGuichets,int nbClients,int[] tabJetonsServices);
     public native int[] ou_sont_les_clients(int nbEtapes,int nbClients);
@@ -87,7 +55,6 @@ public class Simulation {
                     end= nbClientEtape==nbClients;
                 }
             }
-            System.out.println("***************************************************************");
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
