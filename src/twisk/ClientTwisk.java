6b3cdec4703ef375e5c7bd9d3c69a192d69efe35
AlAmeri Rawan <rawan.al-ameri9@etu.univ-lorraine.fr>
2022-03-29 16:04:34 +0200

    Ajout un nouveau Monde dans la class ClientTwisk

--- src/twisk/ClientTwisk.java
+++ src/twisk/ClientTwisk.java
@@ -19,6 +19,25 @@ public class ClientTwisk {
         monde.aCommeSortie(Cine);
     }
 
+    
+    public void intialiserMonde2(Monde monde){
+        Etape fileTob,tob,fileBalon,Balonc,plage,fileWC,toilette;
+        fileTob=new Guichet("File Toboggon",3);
+        tob=new ActiviteRestreinte("Toboggan",2,1);
+        fileBalon=new Guichet("File Balançoire");
+        Balonc=new Activite("Balonçoire",8,2);
+        plage=new Activite("Plage");
+        fileWC=new Guichet("File WC");
+        toilette=new Activite("Toilette");
+        fileTob.ajouterSuccesseur(tob);
+        fileBalon.ajouterSuccesseur(Balonc);
+        tob.ajouterSuccesseur(plage);
+        Balonc.ajouterSuccesseur(plage,fileWC);
+        fileWC.ajouterSuccesseur(toilette);
+        monde.ajouter(fileTob,tob,fileBalon,Balonc,plage,fileWC,toilette);
+        monde.aCommeEntree(fileTob,fileBalon);
+        monde.aCommeSortie(plage,Balonc,toilette);
+    }
 
     public static void main(String[] args) {
         Simulation sim = new Simulation();
