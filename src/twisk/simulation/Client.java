9d8a8860016eb4e84814b2af060c7e91c5ffb031
AlAmeri Rawan <rawan.al-ameri9@etu.univ-lorraine.fr>
2022-03-29 14:29:44 +0200

    Documentation

--- src/twisk/simulation/Client.java
+++ src/twisk/simulation/Client.java
@@ -1,17 +1,28 @@
 package twisk.simulation;
 
 import twisk.monde.Etape;
-
+/**
+ * Classe Client
+ * @author IKHRICHI SOUMAYA AL-AMERI RAWAN
+ */
 public class Client {
     private int numeroClient;
     private int rang;
     private Etape etape;
 
-
+    /**
+     * la constructeur du client
+     * @param numero le numéro du client
+     */
     public Client(int numero){
         this.numeroClient = numero;
     }
 
+    /**
+     * met à jour les attributs etape et rang d’un client
+     * @param etape l'etape dans le monde
+     * @param rang le rang d'un client
+     */
     public void allerA(Etape etape, int rang)
         {
             this.etape=etape;
